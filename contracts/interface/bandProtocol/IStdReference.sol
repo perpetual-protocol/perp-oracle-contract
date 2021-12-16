369dae1a0d5966df4774744648b06509ee3cfc5f
Vinta Chen <vinta.chen@gmail.com>
2021-12-16 12:28:14 +0800

    fix linting

--- contracts/interface/bandProtocol/IStdReference.sol
+++ contracts/interface/bandProtocol/IStdReference.sol
@@ -12,10 +12,7 @@ interface IStdReference {
     }
 
     /// Returns the price data for the given base/quote pair. Revert if not available.
-    function getReferenceData(string memory _base, string memory _quote)
-        external
-        view
-        returns (ReferenceData memory);
+    function getReferenceData(string memory _base, string memory _quote) external view returns (ReferenceData memory);
 
     /// Similar to getReferenceData, but with multiple base/quote pairs at once.
     function getReferenceDataBulk(string[] memory _bases, string[] memory _quotes)
