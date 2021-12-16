9141c1fe3a111d051d5fdf32e9911af872c1a4a3
Vinta Chen <vinta.chen@gmail.com>
2021-12-16 12:28:02 +0800

    WIP: add updateLatestPriceData() and getPrice()

--- contracts/BandPriceFeed.sol
+++ contracts/BandPriceFeed.sol
@@ -10,43 +10,119 @@ import { IStdReference } from "./interface/bandProtocol/IStdReference.sol";
 contract BandPriceFeed is IPriceFeed, BlockContext {
     using Address for address;
 
+    //
+    // STRUCT
+    //
+
+    struct Observation {
+        uint256 priceCumulative;
+        uint256 timestamp;
+    }
+
     string public constant QUOTE_ASSET = "USD";
 
-    IStdReference public priceFeedRef;
     string public baseAsset;
-    uint256 public latestTimestamp;
+    IStdReference public stdRef;
+    Observation[] public observations; // TODO: restrict array size
 
-    event PriceUpdated(uint256 price, uint256 timestamp);
+    event PriceUpdated(string indexed baseAsset, uint256 price, uint256 timestamp);
 
     //
-    // FUNCTIONS
+    // EXTERNAL NON-VIEW
     //
-    constructor(IStdReference ref, string memory baseAssetArg) {
+
+    constructor(IStdReference stdRefArg, string memory baseAssetArg) {
         // BPF_ANC: Reference address is not contract
-        require(address(ref).isContract(), "BPF_ANC");
+        require(address(stdRefArg).isContract(), "BPF_ANC");
 
-        priceFeedRef = ref;
+        stdRef = stdRefArg;
         baseAsset = baseAssetArg;
     }
 
-    function decimals() external pure override returns (uint8) {
-        return 18;
+    /// @dev should be called by a keeper
+    function updateLatestPriceData() external {
+        Observation memory lastObservation = observations[observations.length - 1];
+
+        IStdReference.ReferenceData memory bandData = stdRef.getReferenceData(baseAsset, QUOTE_ASSET);
+        // BPF_ITQ: invalid timestamp for quote
+        require(bandData.lastUpdatedQuote > 0, "BPF_ITQ");
+        // BPF_ITB: invalid timestamp for base
+        require(bandData.lastUpdatedBase > lastObservation.timestamp, "BPF_ITB");
+        // BPF_IP: invalid price
+        require(bandData.rate >= 0, "BPF_IP");
+
+        uint256 elapsedTime = bandData.lastUpdatedBase - lastObservation.timestamp;
+        observations.push(
+            Observation({
+                priceCumulative: lastObservation.priceCumulative + (bandData.rate * elapsedTime),
+                timestamp: bandData.lastUpdatedBase
+            })
+        );
+
+        emit PriceUpdated(baseAsset, bandData.rate, bandData.lastUpdatedBase);
     }
+
+    //
+    // EXTERNAL VIEW
+    //
 
     function getPrice(uint256 interval) external view override returns (uint256) {
+        if (interval == 0) {
+            IStdReference.ReferenceData memory bandData = stdRef.getReferenceData(baseAsset, QUOTE_ASSET);
+            return bandData.rate;
+        }
+
+        // TODO: not enough history data
+        uint256 targetTimestamp = _blockTimestamp() - interval;
+        uint256 index = observations.length - 1;
+        uint256 beforeOrAtIndex;
+        uint256 atOrAfterIndex;
+        while (true) {
+            if (observations[index].timestamp <= targetTimestamp) {
+                beforeOrAtIndex = index;
+                atOrAfterIndex = beforeOrAtIndex + 1;
+                if (atOrAfterIndex >= observations.length) {
+                    atOrAfterIndex = beforeOrAtIndex;
+                }
+                break;
+            }
+            index = index - 1;
+        }
+
+        // TODO: if end == beforeOrAtTarget == atOrAfterTarget
+        // TODO: should we query bandData to calculate end?
+        Observation memory end = observations[observations.length - 1];
+        Observation memory beforeOrAtTarget = observations[beforeOrAtIndex];
+        Observation memory atOrAfterTarget = observations[atOrAfterIndex];
+
+        uint256 targetPriceCumulative;
+        if (targetTimestamp == beforeOrAtTarget.timestamp) {
+            // we're at the left boundary
+            targetPriceCumulative = beforeOrAtTarget.priceCumulative;
+        } else if (targetTimestamp == atOrAfterTarget.timestamp) {
+            // we're at the right boundary
+            targetPriceCumulative = atOrAfterTarget.priceCumulative;
+        } else {
+            // we're in the middle
+            uint256 observationTimeDelta = atOrAfterTarget.timestamp - beforeOrAtTarget.timestamp;
+            uint256 targetTimeDelta = targetTimestamp - beforeOrAtTarget.timestamp;
+            targetPriceCumulative =
+                beforeOrAtTarget.priceCumulative +
+                ((atOrAfterTarget.priceCumulative - beforeOrAtTarget.priceCumulative) / observationTimeDelta) *
+                targetTimeDelta;
+        }
+
+        // TODO: if twap <= 0?
+        uint256 twap = (end.priceCumulative - targetPriceCumulative) / (end.timestamp - targetTimestamp);
+        return twap;
     }
 
-    // EXTERNAL
+    //
+    // EXTERNAL PURE
+    //
 
-    function updateLatestRoundData() external {
-        IStdReference.ReferenceData memory data = priceFeedRef.getReferenceData(baseAsset, QUOTE_ASSET);
-        require(data.lastUpdatedQuote > 0, "BPF_ITQ");
-        require(data.lastUpdatedBase > latestTimestamp, "BPF_ITB");
-        require(data.rate >= 0, "negative price");
-
-        emit PriceUpdated(data.rate, data.lastUpdatedBase);
-
-        latestTimestamp = data.lastUpdatedBase;
+    function decimals() external pure override returns (uint8) {
+        return 18;
     }
 
     //
