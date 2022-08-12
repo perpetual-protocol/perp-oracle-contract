049fb745497de61d432519afac10ee2da2ab44e9
Opass Chang <th33ts@gmail.com>
2022-08-12 14:22:11 +0800

    Implement PriceFeedUpdater

--- contracts/PriceFeedUpdater.sol
+++ contracts/PriceFeedUpdater.sol
@@ -3,13 +3,18 @@ pragma solidity 0.7.6;
 
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { IPriceFeedUpdate } from "./interface/IPriceFeedUpdate.sol";
+import { SafeOwnableNonUpgradable } from "./base/SafeOwnableNonUpgradable.sol";
 
-contract PriceFeedUpdater {
+contract PriceFeedUpdater is SafeOwnableNonUpgradable {
     using Address for address;
 
     address[] public priceFeeds;
 
     constructor(address[] memory priceFeedsArg) {
+        setPriceFeeds(priceFeedsArg);
+    }
+
+    function setPriceFeeds(address[] memory priceFeedsArg) public onlyOwner {
         // PFU_PFANC: price feed address is not contract
         for (uint256 i = 0; i < priceFeedsArg.length; i++) {
             require(priceFeedsArg[i].isContract(), "PFU_PFANC");
@@ -18,5 +23,9 @@ contract PriceFeedUpdater {
         priceFeeds = priceFeedsArg;
     }
 
-    fallback() external {}
+    fallback() external {
+        for (uint256 i = 0; i < priceFeeds.length; i++) {
+            try IPriceFeedUpdate(priceFeeds[i]).update() {} catch {}
+        }
+    }
 }
