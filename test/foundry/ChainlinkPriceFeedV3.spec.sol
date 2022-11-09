68b15a6c6e1921b58afa56e8d8dcbb3ce3926de3
tienshaoku <tienshaoku@gmail.com>
2022-11-09 16:47:20 +0800

    # IncorrectDecimals case added.

--- test/foundry/ChainlinkPriceFeedV3.spec.sol
+++ test/foundry/ChainlinkPriceFeedV3.spec.sol
@@ -8,11 +8,19 @@ import "../../contracts/interface/IPriceFeedV3.sol";
 
 contract ChainlinkPriceFeedV3Test is IPriceFeedV3Event, BaseSetup {
     uint256 internal _timestamp = 10000000;
+    uint256 internal _price = 1000 * 1e8;
+    uint256 internal _roundId = 1;
 
     function setUp() public virtual override {
         BaseSetup.setUp();
         vm.warp(_timestamp);
         vm.mockCall(address(_testAggregator), abi.encodeWithSelector(_testAggregator.decimals.selector), abi.encode(8));
+
+        vm.mockCall(
+            address(_testAggregator),
+            abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
+            abi.encode(_roundId, _price, _timestamp, _timestamp, _roundId)
+        );
     }
 
     function test_getAggregator() public {
@@ -41,37 +49,22 @@ contract ChainlinkPriceFeedV3Test is IPriceFeedV3Event, BaseSetup {
     }
 
     function test_cachePrice_first_call_with_valid_price() public {
-        uint256 price = 1000 * 1e8;
-        uint256 roundId = 1;
-        vm.mockCall(
-            address(_testAggregator),
-            abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
-            abi.encode(roundId, price, _timestamp, _timestamp, roundId)
-        );
-
         vm.expectEmit(false, false, false, true, address(_chainlinkPriceFeedV3));
-        emit PriceUpdated(price, _timestamp, FreezedReason.NotFreezed);
-        assertEq(_chainlinkPriceFeedV3.cachePrice(), price);
-        assertEq(_chainlinkPriceFeedV3.getLastValidPrice(), price);
+        emit PriceUpdated(_price, _timestamp, FreezedReason.NotFreezed);
+        assertEq(_chainlinkPriceFeedV3.cachePrice(), _price);
+        assertEq(_chainlinkPriceFeedV3.getLastValidPrice(), _price);
     }
 
     function test_cachePrice_should_return__lastValidPrice() public {
-        uint256 price1 = 1000 * 1e8;
-        uint256 price2 = 2000 * 1e8;
-        uint256 roundId = 1;
-        vm.mockCall(
-            address(_testAggregator),
-            abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
-            abi.encode(roundId, price1, _timestamp, _timestamp, roundId)
-        );
         _chainlinkPriceFeedV3.cachePrice();
 
+        uint256 price2 = 2000 * 1e8;
         vm.mockCall(
             address(_testAggregator),
             abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
-            abi.encode(roundId, price2, _timestamp, _timestamp, roundId)
+            abi.encode(_roundId, price2, _timestamp, _timestamp, _roundId)
         );
-        assertEq(_chainlinkPriceFeedV3.cachePrice(), price1);
+        assertEq(_chainlinkPriceFeedV3.cachePrice(), _price);
     }
 
     function test_cachePrice_freezedReason_is_NoResponse() public {
@@ -82,5 +75,13 @@ contract ChainlinkPriceFeedV3Test is IPriceFeedV3Event, BaseSetup {
         assertEq(_chainlinkPriceFeedV3Broken.cachePrice(), 0);
         assertEq(_chainlinkPriceFeedV3Broken.getLastValidTime(), 0);
         assertEq(int256(_chainlinkPriceFeedV3Broken.getFreezedReason()), int256(FreezedReason.NoResponse));
+    }
+
+    function test_cachePrice_freezedReason_is_IncorrectDecimals() public {
+        vm.mockCall(address(_testAggregator), abi.encodeWithSelector(_testAggregator.decimals.selector), abi.encode(7));
+
+        vm.expectEmit(false, false, false, true, address(_chainlinkPriceFeedV3));
+        emit PriceUpdated(0, 0, FreezedReason.IncorrectDecimals);
+        _chainlinkPriceFeedV3.cachePrice();
     }
 }
