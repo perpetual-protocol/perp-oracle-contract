8481a2b6edc6675499ed3a25bf00f91c037b4d5e
tienshaoku <tienshaoku@gmail.com>
2022-11-09 16:34:39 +0800

    # Two test cases added.

--- test/foundry/BaseSetup.sol
+++ test/foundry/BaseSetup.sol
@@ -4,27 +4,80 @@ import "forge-std/Test.sol";
 import { TestAggregatorV3 } from "../../contracts/test/TestAggregatorV3.sol";
 import { ChainlinkPriceFeedV3 } from "../../contracts/ChainlinkPriceFeedV3.sol";
 
+contract AggregatorV3Broken is TestAggregatorV3 {
+    function latestRoundData()
+        external
+        view
+        override
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        )
+    {
+        revert();
+    }
+
+    function decimals() external view override returns (uint8) {
+        revert();
+    }
+}
+
+contract ChainlinkPriceFeedV3Broken is ChainlinkPriceFeedV3 {
+    constructor(
+        TestAggregatorV3 aggregator,
+        uint256 timeout,
+        uint24 maxOutlierDeviationRatio,
+        uint256 outlierCoolDownPeriod
+    ) ChainlinkPriceFeedV3(aggregator, timeout, maxOutlierDeviationRatio, outlierCoolDownPeriod) {}
+
+    function getFreezedReason() public returns (FreezedReason) {
+        return _getFreezedReason(_getChainlinkData());
+    }
+}
+
 contract BaseSetup is Test {
-    uint256 internal constant _timeout = 40 * 60;
-    TestAggregatorV3 internal testAggregator;
-    ChainlinkPriceFeedV3 internal chainlinkPriceFeedV3;
+    uint256 internal constant _TIMEOUT = 40 * 60;
+
+    TestAggregatorV3 internal _testAggregator;
+    ChainlinkPriceFeedV3 internal _chainlinkPriceFeedV3;
+
+    AggregatorV3Broken internal _aggregatorV3Broken;
+    ChainlinkPriceFeedV3Broken internal _chainlinkPriceFeedV3Broken;
 
     function setUp() public virtual {
-        testAggregator = createTestAggregator();
-        chainlinkPriceFeedV3 = createChainlinkPriceFeedV3();
+        _testAggregator = _createTestAggregator();
+        _chainlinkPriceFeedV3 = _createChainlinkPriceFeedV3();
+
+        _aggregatorV3Broken = _createAggregatorBroken();
+        _chainlinkPriceFeedV3Broken = __createChainlinkPriceFeedV3Broken();
+
+        vm.clearMockedCalls();
     }
 
     //
     // INTERNAL VIEW
     //
 
-    function createTestAggregator() internal returns (TestAggregatorV3) {
+    function _createTestAggregator() internal returns (TestAggregatorV3) {
         TestAggregatorV3 aggregator = new TestAggregatorV3();
         vm.mockCall(address(aggregator), abi.encodeWithSelector(aggregator.decimals.selector), abi.encode(8));
         return aggregator;
     }
 
-    function createChainlinkPriceFeedV3() internal returns (ChainlinkPriceFeedV3) {
-        return new ChainlinkPriceFeedV3(testAggregator, _timeout, 1e5, 10);
+    function _createChainlinkPriceFeedV3() internal returns (ChainlinkPriceFeedV3) {
+        return new ChainlinkPriceFeedV3(_testAggregator, _TIMEOUT, 1e5, 10);
+    }
+
+    function _createAggregatorBroken() internal returns (AggregatorV3Broken) {
+        AggregatorV3Broken aggregator = new AggregatorV3Broken();
+        vm.mockCall(address(aggregator), abi.encodeWithSelector(aggregator.decimals.selector), abi.encode(8));
+        return aggregator;
+    }
+
+    function __createChainlinkPriceFeedV3Broken() internal returns (ChainlinkPriceFeedV3Broken) {
+        return new ChainlinkPriceFeedV3Broken(_aggregatorV3Broken, _TIMEOUT, 1e5, 10);
     }
 }
--- test/foundry/ChainlinkPriceFeedV3.spec.sol
+++ test/foundry/ChainlinkPriceFeedV3.spec.sol
@@ -12,45 +12,75 @@ contract ChainlinkPriceFeedV3Test is IPriceFeedV3Event, BaseSetup {
     function setUp() public virtual override {
         BaseSetup.setUp();
         vm.warp(_timestamp);
+        vm.mockCall(address(_testAggregator), abi.encodeWithSelector(_testAggregator.decimals.selector), abi.encode(8));
     }
 
     function test_getAggregator() public {
-        assertEq(chainlinkPriceFeedV3.getAggregator(), address(testAggregator));
+        assertEq(_chainlinkPriceFeedV3.getAggregator(), address(_testAggregator));
     }
 
     function test_getLastValidPrice_is_0_when_initialized() public {
-        assertEq(chainlinkPriceFeedV3.getLastValidPrice(), 0);
+        assertEq(_chainlinkPriceFeedV3.getLastValidPrice(), 0);
     }
 
     function test_getLastValidTime_is_0_when_initialized() public {
-        assertEq(chainlinkPriceFeedV3.getLastValidTime(), 0);
+        assertEq(_chainlinkPriceFeedV3.getLastValidTime(), 0);
     }
 
     function test_decimals() public {
-        assertEq(uint256(chainlinkPriceFeedV3.decimals()), uint256(testAggregator.decimals()));
+        assertEq(uint256(_chainlinkPriceFeedV3.decimals()), uint256(_testAggregator.decimals()));
     }
 
     function test_isTimedOut_is_false_when_initialized() public {
-        assertEq(chainlinkPriceFeedV3.isTimedOut(), false);
+        assertEq(_chainlinkPriceFeedV3.isTimedOut(), false);
     }
 
     function test_isTimedOut() public {
-        vm.warp(_timeout - 1);
-        assertEq(chainlinkPriceFeedV3.isTimedOut(), true);
+        vm.warp(_TIMEOUT - 1);
+        assertEq(_chainlinkPriceFeedV3.isTimedOut(), true);
     }
 
     function test_cachePrice_first_call_with_valid_price() public {
         uint256 price = 1000 * 1e8;
         uint256 roundId = 1;
         vm.mockCall(
-            address(testAggregator),
-            abi.encodeWithSelector(testAggregator.latestRoundData.selector),
+            address(_testAggregator),
+            abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
             abi.encode(roundId, price, _timestamp, _timestamp, roundId)
         );
 
-        vm.expectEmit(false, false, false, true, address(chainlinkPriceFeedV3));
+        vm.expectEmit(false, false, false, true, address(_chainlinkPriceFeedV3));
         emit PriceUpdated(price, _timestamp, FreezedReason.NotFreezed);
-        assertEq(chainlinkPriceFeedV3.cachePrice(), price);
-        assertEq(chainlinkPriceFeedV3.getLastValidPrice(), price);
+        assertEq(_chainlinkPriceFeedV3.cachePrice(), price);
+        assertEq(_chainlinkPriceFeedV3.getLastValidPrice(), price);
+    }
+
+    function test_cachePrice_should_return__lastValidPrice() public {
+        uint256 price1 = 1000 * 1e8;
+        uint256 price2 = 2000 * 1e8;
+        uint256 roundId = 1;
+        vm.mockCall(
+            address(_testAggregator),
+            abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
+            abi.encode(roundId, price1, _timestamp, _timestamp, roundId)
+        );
+        _chainlinkPriceFeedV3.cachePrice();
+
+        vm.mockCall(
+            address(_testAggregator),
+            abi.encodeWithSelector(_testAggregator.latestRoundData.selector),
+            abi.encode(roundId, price2, _timestamp, _timestamp, roundId)
+        );
+        assertEq(_chainlinkPriceFeedV3.cachePrice(), price1);
+    }
+
+    function test_cachePrice_freezedReason_is_NoResponse() public {
+        // cannot expect event here bc of foundry's bug
+        // vm.expectEmit(false, false, false, false, address(_chainlinkPriceFeedV3Broken));
+        // emit PriceUpdated(0, 0, FreezedReason.NoResponse);
+
+        assertEq(_chainlinkPriceFeedV3Broken.cachePrice(), 0);
+        assertEq(_chainlinkPriceFeedV3Broken.getLastValidTime(), 0);
+        assertEq(int256(_chainlinkPriceFeedV3Broken.getFreezedReason()), int256(FreezedReason.NoResponse));
     }
 }
