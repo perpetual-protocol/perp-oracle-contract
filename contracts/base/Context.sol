68c32ce9350f44e3557bd3cdeaca9b3f84a65d34
twxia <passion804222@gmail.com>
2022-11-23 16:44:56 +0800

    chore: remove comment from Context.sol

--- contracts/base/Context.sol
+++ contracts/base/Context.sol
@@ -18,7 +18,7 @@ abstract contract Context {
     }
 
     function _msgData() internal view virtual returns (bytes memory) {
-        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        this;
         return msg.data;
     }
 }
